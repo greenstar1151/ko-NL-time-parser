4f02d427a02155ca34888cd08f489ee86abc204c
greenstar1151 <39073901+greenstar1151@users.noreply.github.com>
2021-02-07 22:28:13 +0900

    Improve parser

--- ko_NL_time_parser.py
+++ ko_NL_time_parser.py
@@ -1,17 +1,21 @@
-import copy
 import inspect
 import types
 from datetime import datetime, timedelta
 from pprint import pprint
 
-from dataset import dataset
+try:
+    from .dataset import dataset
+except ImportError:
+    from dataset import dataset
 
+debug = True
 
 class timeObject():
-    def __init__(self, exec, meta, name = '') -> None:
-        self.exec = exec
+    def __init__(self, expression, meta, time_stack, name = '') -> None:
+        self.expression = expression
         self.frontW = int(meta[0])
         self.backW = int(meta[1])
+        self.time_stack = time_stack
         self.name = name
     
     def getTotalW(self) -> int:
@@ -23,12 +27,6 @@ class timeObject():
 
     def __repr__(self) -> str:
         return f'|{self.frontW}|-timeObject({self.name})-|{self.backW}|'
-        if type(self.exec) == types.LambdaType:
-            l = str(inspect.getsourcelines(self.exec)[0])
-            #l = l.split("'exec':")[1].strip()[:-5] # temp
-            return f'|{self.frontW}|-timeObject({l})-|{self.backW}|'
-        else:
-            return f'|{self.frontW}|-timeObject({self.exec})-|{self.backW}|'
 
 
 def to_time(token: str) -> list:
@@ -53,62 +51,106 @@ def to_time(token: str) -> list:
     for i, tok in enumerate(parsed):
         if type(tok) == str:
             try:
-                timeObj = timeObject(dataset[tok]['exec'], dataset[tok]['meta'], tok)
+                timeObj = timeObject(dataset[tok]['exps'], dataset[tok]['meta'], dataset[tok]['time_stack'], tok)
                 parsed[i] = timeObj
             except KeyError:
+                parsed[i] = None
+                continue
                 raise NotImplementedError # TODO: temp
         elif type(tok) == float:
-            timeObj = timeObject(tok, (1, 0), str(tok))
+            timeObj = timeObject(tok, (1, 0), True, str(tok))
             parsed[i] = timeObj
-           
+    
+    parsed = list(filter(lambda x: x != None, parsed))
     return parsed
 
 
 def parse_time(text, time_base = datetime.now()):
-    '''Main parser function'''
+    '''Parse korean NL time expression to datetime'''
     words = text.split(' ') # Basic tokenizing(whitespace)
+    words = filter(lambda x: len(x) > 0, words)
     translated = []
     for word in words: # Translate to timeObject
         word_translated = to_time(word)
         translated += word_translated
-    #pprint(translated)
+    if debug: pprint(translated)
 
     optimized = []
     stack = []
     stack_name = []
-    for tObj in translated: # Optimize timeObject [ex] [(1, 0), (0, 0), (0, 1)] -> [(1, 1)]
-        #print(f'forloop: {tObj}, {tObj.getTotalW()}')
-        
-        if tObj.getTotalW() == 1:
-            optimized.append(tObj)
-        else:
-            if len(stack) != 0:
-                last = stack.pop()
-                if type(last) == timeObject:
-                    stack_name.append(tObj.name)
-                    stack.append(tObj.exec(last.exec))
-                else:
-                    stack_name.append(tObj.name)
-                    stack.append(timeObject(tObj.exec(last), (1, 1), ' '.join(stack_name)))
-            else:
-                stack.append(tObj)
-                stack_name.append(tObj.name)
+    stack_time = []
+    for i in range(len(translated) - 1):
+        currObj = translated[i]
+        nextObj = translated[i + 1]
+        if currObj.getTotalW() == 1:
+            optimized.append(currObj)
+            continue
+        if currObj.backW + nextObj.frontW == 0:
+            name = f'{currObj.name}{nextObj.name}'
+            newObj = timeObject(nextObj.expression(currObj.expression), 
+                                (1, 1), 
+                                currObj.time_stack and nextObj.time_stack,
+                                name)
+            stack_time.append(newObj)
+            stack_name.append(name)
+        elif nextObj.frontW == 0:
+            stack.append(nextObj)
     else:
-        optimized += stack
-    #pprint(optimized)
+        if len(translated) > 0:
+            if translated[-1].getTotalW() == 1:
+                optimized.append(translated[-1])
+    
+    timeDeltaSum = timedelta(0)
+    while len(stack_time) != 0:
+        tObj = stack_time.pop()
+        timeDeltaSum += tObj.expression
+    
+    if len(stack) == 1:
+        tAlter = stack.pop()
+        tAlter.expression(timeDeltaSum)
+        tObj = timeObject(tAlter.expression(timeDeltaSum),
+                          (1, 1),
+                          False,
+                          ' '.join(stack_name + [tAlter.name]))
+        optimized.append(tObj)
+
+    # for tObj in translated: # Optimize timeObject [ex] [(1, 0), (0, 0), (0, 1)] -> [(1, 1)]
+    #     print(f'forloop: {tObj}, {tObj.getTotalW()}')
+        
+    #     if tObj.getTotalW() == 1:
+    #         optimized.append(tObj)
+    #     else:
+    #         if len(stack) != 0:
+    #             last = stack.pop()
+    #             stack_name.append(tObj.name)
+    #             stack.append(timeObject(tObj.expression(last), (1, 1), ' '.join(stack_name)))
+    #             # if type(last) == timeObject:
+    #             #     stack_name.append(tObj.name)
+    #             #     stack.append(tObj.expression(last.expression))
+    #             # else:
+    #             #     stack_name.append(tObj.name)
+    #             #     stack.append(timeObject(tObj.expression(last), (1, 1), ' '.join(stack_name)))
+    #         else:
+    #             stack.append(tObj)
+    #             stack_name.append(tObj.name)
+    # else:
+    #     optimized += stack
+    if debug: pprint(optimized)
 
     for tDelta in optimized:
-        time_base = tDelta.exec(time_base)
+        time_base = tDelta.expression(time_base)
 
-    return time_base
+    return time_base, translated, optimized
 
 
 
 if __name__ == '__main__':
-    text = '내일 자정 1분 전'
+    debug = True
+
+    text = '1시간 23분 45초 후'
     time_base = datetime.now()
 
     print(f'현재 시각: {time_base}')
     print(f'[In] {text}')
 
-    print(f'[Out] {parse_time(text, time_base)}')
+    print(f'[Out] {parse_time(text, time_base)[0]}')
