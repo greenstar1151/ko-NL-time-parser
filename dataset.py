557f5c99a08cf1b040af2f14f879feba51735a13
greenstar1151 <39073901+greenstar1151@users.noreply.github.com>
2021-02-07 22:28:17 +0900

    Update dataset corpus

--- dataset.py
+++ dataset.py
@@ -1,4 +1,6 @@
-from datetime import timedelta, datetime
+from datetime import datetime, timedelta
+
+from dateutil.relativedelta import relativedelta
 
 
 def ceil_day(t: datetime) -> datetime:
@@ -7,56 +9,127 @@ def ceil_day(t: datetime) -> datetime:
 def mid_day(t: datetime) -> datetime:
     return t.replace(day = t.day, hour=12, minute=0, second=0, microsecond=0)
 
+def one_month(t: datetime) -> datetime:
+    t = t+relativedelta(months=+1)
+    return t
+
 
 dataset = {
     '오늘': {
         'type': 'independent',
-        'exec': lambda t: t + timedelta(0),
-        'meta': (1, 1)
+        'exps': lambda t: t + timedelta(0),
+        'meta': (1, 1),
+        'time_stack': False
+    },
+    '지금': {
+        'type': 'independent',
+        'exps': lambda t: t + timedelta(0),
+        'meta': (1, 1),
+        'time_stack': False
     },
     '내일': {
         'type': 'independent',
-        'exec': lambda t: t + timedelta(1),
-        'meta': (1, 1)
+        'exps': lambda t: t + timedelta(1),
+        'meta': (1, 1),
+        'time_stack': False
     },
+    '모레': {
+        'type': 'independent',
+        'exps': lambda t: t + timedelta(2),
+        'meta': (1, 1),
+        'time_stack': False
+    },
+    '글피': {
+        'type': 'independent',
+        'exps': lambda t: t + timedelta(3),
+        'meta': (1, 1),
+        'time_stack': False
+    },
+    '사흘': {
+        'type': 'independent',
+        'exps': lambda t: t + timedelta(3),
+        'meta': (1, 1),
+        'time_stack': False
+    },
+    '나흘': {
+        'type': 'independent',
+        'exps': lambda t: t + timedelta(4),
+        'meta': (1, 1),
+        'time_stack': False
+    },
+    '일주일': {
+        'type': 'independent',
+        'exps': lambda t: t + timedelta(7),
+        'meta': (1, 1),
+        'time_stack': False
+    },
+    '한달': {
+        'type': 'independent',
+        'exps': lambda t: one_month(t),
+        'meta': (1, 1),
+        'time_stack': False
+    },
+
     '정오': {
         'type': 'independent',
-        'exec': lambda t: mid_day(t),
-        'meta': (1, 1)
+        'exps': lambda t: mid_day(t),
+        'meta': (1, 1),
+        'time_stack': False
     },
     '자정': {
         'type': 'independent',
-        'exec': lambda t: ceil_day(t),
-        'meta': (1, 1)
-    },\
+        'exps': lambda t: ceil_day(t),
+        'meta': (1, 1),
+        'time_stack': False
+    },
+
     '초': {
         'type': 'dependent',
-        'exec': lambda x: timedelta(0, x),
-        'meta': (0, 0)
+        'exps': lambda x: timedelta(0, x),
+        'meta': (0, 1),
+        'time_stack': True
     },
     '분': {
         'type': 'dependent',
-        'exec': lambda x: timedelta(0, 60 * x),
-        'meta': (0, 0)
+        'exps': lambda x: timedelta(0, 60 * x),
+        'meta': (0, 1),
+        'time_stack': True
     },
     '시간': {
         'type': 'dependent',
-        'exec': lambda x: timedelta(0, 3600 * x),
-        'meta': (0, 0)
+        'exps': lambda x: timedelta(0, 3600 * x),
+        'meta': (0, 1),
+        'time_stack': True
     },
     '일': {
         'type': 'dependent',
-        'exec': lambda x: timedelta(x),
-        'meta': (0, 0)
+        'exps': lambda x: timedelta(x),
+        'meta': (0, 1),
+        'time_stack': True
     },
+    '주일': {
+        'type': 'dependent',
+        'exps': lambda x: timedelta(7*x),
+        'meta': (0, 1),
+        'time_stack': True
+    },
+
     '전': {
         'type': 'dependent',
-        'exec': lambda x: (lambda t: t - x),
-        'meta': (0, 1)
+        'exps': lambda x: (lambda t: t - x),
+        'meta': (0, 1),
+        'time_stack': False
     },
     '후': {
         'type': 'dependent',
-        'exec': lambda x: (lambda t: t + x),
-        'meta': (0, 1)
+        'exps': lambda x: (lambda t: t + x),
+        'meta': (0, 1),
+        'time_stack': False
+    },
+    '뒤': {
+        'type': 'dependent',
+        'exps': lambda x: (lambda t: t + x),
+        'meta': (0, 1),
+        'time_stack': False
     }
 }
